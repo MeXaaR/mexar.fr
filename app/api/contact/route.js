5402e257cf6e3302876ae0df2f0459324a0ecef8
Francois AUBEUT <contact@mexar.fr>
2025-02-12 17:15:42 +0100

    remove security

--- app/api/contact/route.js
+++ app/api/contact/route.js
@@ -1,15 +1,15 @@
 import contactInfos from "@/data/contactInfos";
 
 export async function GET(request) {
-    const apiKey = process.env.API_KEY;
-    const providedKey = request.headers.get("x-api-key");
+    // const apiKey = process.env.API_KEY;
+    // const providedKey = request.headers.get("x-api-key");
 
-    if (!apiKey || !providedKey || apiKey !== providedKey) {
-        return new Response(
-            { error: "Unauthorized - Invalid or missing API key" },
-            { status: 401 }
-        );
-    }
+    // if (!apiKey || !providedKey || apiKey !== providedKey) {
+    //     return new Response(
+    //         { error: "Unauthorized - Invalid or missing API key" },
+    //         { status: 401 }
+    //     );
+    // }
 
     return new Response(JSON.stringify({
         contact: contactInfos,
