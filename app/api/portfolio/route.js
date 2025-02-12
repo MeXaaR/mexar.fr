ba898fceb93936ed4fc1420ff26d519710ab9b85
Francois AUBEUT <contact@mexar.fr>
2025-02-12 17:06:53 +0100

    fix routes

--- app/api/portfolio/route.js
+++ app/api/portfolio/route.js
@@ -1,18 +1,17 @@
 import { portfolioSection } from "@/data/portfolio/portfolioSection";
-import { NextResponse } from "next/server";
 
 export async function GET(request) {
     const apiKey = process.env.API_KEY;
     const providedKey = request.headers.get("x-api-key");
 
     if (!apiKey || !providedKey || apiKey !== providedKey) {
-        return NextResponse.json(
+        return new Response(
             { error: "Unauthorized - Invalid or missing API key" },
             { status: 401 }
         );
     }
 
-    return NextResponse.json({
+    return new Response(JSON.stringify({
         portfolio: portfolioSection,
-    });
+    }));
 }
